01a591bab02de863bb3743e787a4cc71ce0b6e9d
Roman Kravtsov <kravtsov.official@gmail.com>
2019-07-14 00:18:37 +0200

    Fix footer color

--- client/src/components/Footer.vue
+++ client/src/components/Footer.vue
@@ -1,11 +1,11 @@
 <template>
   <v-footer color="primary lighten-1" padless :absolute="true">
     <v-card class="flex" flat tile>
-      <v-card-title class="blue-grey lighten-2">
+      <v-card-title class="teal lighten-3">
         <strong class="subheading">Get connected with us on social networks!</strong>
         <v-spacer></v-spacer>
         <v-btn v-for="icon in icons" :key="icon" class="mx-3" dark icon>
-          <v-icon size="24px">{{ icon }}</v-icon>
+          <v-icon size="36px">{{ icon }}</v-icon>
         </v-btn>
       </v-card-title>
     </v-card>
