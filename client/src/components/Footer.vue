e0775422eb373a57a9032ec44c27c3500e8afeec
Roman Kravtsov <kravtsov.official@gmail.com>
2019-07-14 12:43:59 +0200

    Rework footer

--- client/src/components/Footer.vue
+++ client/src/components/Footer.vue
@@ -1,22 +1,16 @@
 <template>
-  <v-footer color="primary lighten-1" padless :app="true">
-    <v-card class="flex" flat tile>
-      <v-card-title class="teal lighten-3">
-        <strong class="subheading">Get connected with us on social networks!</strong>
-        <v-spacer />
-        <v-btn v-for="icon in icons" :key="icon" class="mb-2" dark icon>
-          <v-icon size="36px">{{ icon }}</v-icon>
-        </v-btn>
-      </v-card-title>
-    </v-card>
+  <v-footer class="teal lighten-3" height="5%">
+    <v-layout align-center justify-center row fill-height>
+      <v-btn href="https://github.com/Chewbaccamaster/tweet_analyzer" class="mb-2" light icon>
+        <v-icon size="36px">{{ "fab fa-github" }}</v-icon>
+      </v-btn>
+    </v-layout>
   </v-footer>
 </template>
 
 <script>
 export default {
   name: "Footer",
-  data: () => ({
-    icons: ["fab fa-github"]
-  })
+  data: () => ({})
 };
 </script>
