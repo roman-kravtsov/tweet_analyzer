17336b7d03f4ab2a3fb2fcb25b84e017ab9e936a
Vladyslav Tkachuk <tkachuk.v26@gmail.com>
2019-07-17 21:52:27 +0200

    update requirements; server path fixes, handle too long tweets

--- server/static/prediction.py
+++ server/static/prediction.py
@@ -5,26 +5,34 @@ from gensim.models import Word2Vec
 
 from static.preprocess import clean_tweet
 
-root_path = "./models/"
+root_path = "./models/w2v_cnn/"
+model_file = "w2v_cnn_model.json"
+model_weights_file = "w2v_cnn_weights.h5"
+w2vec_model_file = "word2vec.model"
 
 
 def load_nn_model():
     # load json and create model
-    with open(root_path + 'model.json', 'r') as model_file:
-        loaded_model = model_from_json(model_file.read())
+    with open(root_path + model_file, 'r') as model_reader:
+        loaded_model = model_from_json(model_reader.read())
 
     # load weights into new model
-    loaded_model.load_weights(root_path + "model.h5")
+    loaded_model.load_weights(root_path + model_weights_file)
 
     return loaded_model
 
 
 def load_word2vec_model():
-    return Word2Vec.load(root_path + "word2vec.model")
+    return Word2Vec.load(root_path + w2vec_model_file)
 
 
 def get_features(word2vec_model, tweet_words, max_tweet_len):
     w2v_vector_size = word2vec_model.vector_size
+
+    # cut tweet if it is longer than the longest tweet
+    # we encountered during training
+    if len(tweet_words) > max_tweet_len:
+        tweet_words = tweet_words[:max_tweet_len]
 
     features = np.zeros((max_tweet_len, w2v_vector_size))
 
