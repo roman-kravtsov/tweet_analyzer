2cb8cb66ba2f238592791e0bf1ea0016d02ac186
Vladyslav Tkachuk <tkachuk.v26@gmail.com>
2019-07-20 19:57:51 +0200

    fix import to treat classification as a separate project

--- classification/classifiers/nb_tfidf_preprocessing.py
+++ classification/classifiers/nb_tfidf_preprocessing.py
@@ -4,11 +4,11 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn import metrics
 from sklearn.feature_extraction.text import TfidfVectorizer
 
-from classification.util.data_util import get_data
-from classification.util.features_extractor import update_labels
-from classification.util.plot import plot_confusion_matrix
-from classification.util.preprocessor import preprocess
-from classification.util.save_util import save_classifier
+from util.data_util import get_data
+from util.features_extractor import update_labels
+from util.plot import plot_confusion_matrix
+from util.preprocessor import preprocess
+from util.save_util import save_classifier
 
 save_path_prefix = "models_clf/"
 
--- classification/nn/word2vec_cnn.py
+++ classification/nn/word2vec_cnn.py
@@ -1,8 +1,8 @@
-from classification.util.plot import plot_training_results
-from classification.util.preprocessor import preprocess
-from classification.util.features_extractor import get_word2vec_features, update_labels
-from classification.util.data_util import get_data
-from classification.util.model import get_model_word2vec_cnn
+from util.plot import plot_training_results
+from util.preprocessor import preprocess
+from util.features_extractor import get_word2vec_features, update_labels
+from util.data_util import get_data
+from util.model import get_model_word2vec_cnn
 
 from sklearn.metrics import confusion_matrix, classification_report
 
